4f36dc1663ced5f785f2f357a9d3197a3854f219
LadyHail <ladydeath@o2.pl>
2018-06-29 12:06:57 +0200

    CodeFights - Array Replace

--- arrayReplace/Program.cs
+++ arrayReplace/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace arrayReplace
 {
     class Program
     {
+        public static int[] arrayReplace(int[] inputArray, int elemToReplace, int substitutionElem)
+        {
+            int[] result = inputArray.Select(e => e == elemToReplace ? substitutionElem : e).ToArray();
+            return result;
+        }
+
         static void Main(string[] args)
         {
+            int[] input = { 1, 2, 1 };
+            int replace = 1;
+            int substitution = 3;
+
+            int[] result = arrayReplace(input, replace, substitution);
+            foreach (int item in result)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
         }
     }
 }
