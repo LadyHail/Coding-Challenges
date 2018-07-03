ce3bfef8d82a3ff0973c49af304be4b4a8e8a55e
LadyHail <ladydeath@o2.pl>
2018-07-03 21:34:30 +0200

    CodeFights - circleOfNumbers

---
+++ circleOfNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace circleOfNumbers
+{
+    class Program
+    {
+        public static int circleOfNumbers(int n, int firstNumber)
+        {
+            int even = n / 2;
+            int result = firstNumber > even ? firstNumber - even : even == firstNumber ? 0 : even + firstNumber;
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            int n = 10;
+            int first = 7;
+            Console.WriteLine(circleOfNumbers(n, first));
+        }
+    }
+}
