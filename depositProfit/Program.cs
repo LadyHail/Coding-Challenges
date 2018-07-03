6ebd64ff273eb13b4d80b10c234d331221508fc2
LadyHail <ladydeath@o2.pl>
2018-07-03 21:56:05 +0200

    CodeFights - depositProfit

---
+++ depositProfit/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace depositProfit
+{
+    class Program
+    {
+        public static int depositProfit(int deposit, int rate, int threshold)
+        {
+            int result = 0;
+            double current = deposit;
+            while (current < threshold)
+            {
+                current += (current * rate) / 100;
+                result++;
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            int deposit = 100;
+            int rate = 2;
+            int threshold = 170;
+            Console.WriteLine(depositProfit(deposit, rate, threshold));
+            Console.ReadKey();
+        }
+    }
+}
