8c8c299d86c890a1303fad87aaceb773611b38ce
LadyHail <ladydeath@o2.pl>
2018-07-13 20:58:48 +0200

    CodeFights - longestDigitsPrefix

---
+++ Main/CodeFights/LongestDigitsPrefix.cs
@@ -0,0 +1,41 @@
+﻿/*
+ DESCRIPTION:
+ Given a string, output its longest prefix which contains only digits.
+ Example:
+ For inputString="123aa1", the output should be
+ longestDigitsPrefix(inputString) = "123".
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Main.CodeFights
+{
+    public static class LongestDigitsPrefix
+    {
+        public static string longestDigitsPrefix(string inputString)
+        {
+            List<string> list = new List<string>();
+            string current = "";
+
+            if (Char.IsDigit(inputString[0]))
+            {
+                foreach (char ch in inputString)
+                {
+                    if (Char.IsDigit(ch))
+                    {
+                        current += ch;
+                    }
+                    else
+                    {
+                        if (current != "") list.Add(current);
+                        current = "";
+                    }
+                }
+            }
+            list.Add(current);
+            return list.OrderByDescending(x => x.Length).ToList().FirstOrDefault();
+        }
+    }
+}
