f8583039c7e4c6b276dfabdc262d9eff35e48365
LadyHail <ladydeath@o2.pl>
2018-07-17 19:55:37 +0200

    CodeFights - findEmailDomain

---
+++ Main/CodeFights/FindEmailDomain.cs
@@ -0,0 +1,23 @@
+﻿/* 
+ DESCRIPTION:
+ An email address such as "John.Smith@example.com" is made up of a local part ("John.Smith"), an "@" symbol, then a domain part ("example.com").
+ The domain name part of an email address may only consist of letters, digits, hyphens and dots. The local part, however, also allows a lot of different special characters. Here you can look at several examples of correct and incorrect email addresses.
+ Given a valid email address, find its domain part.
+ Example
+     For address = "prettyandsimple@example.com", the output should be
+    findEmailDomain(address) = "example.com";
+    For address = "<>[]:,;@"!#$%&*+-/=?^_{}| ~.a"@example.org", the output should be
+    findEmailDomain(address) = "example.org".
+*/
+
+namespace Main.CodeFights
+{
+    public static class FindEmailDomain
+    {
+        public static string findEmailDomain(string address)
+        {
+            int index = address.LastIndexOf('@');
+            return address.Substring(index + 1);
+        }
+    }
+}
---
+++ Main/CodeFights/IsBeautifulString.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace Main.CodeFights
+{
+    public static class IsBeautifulString
+    {
+        public static bool isBeautifulString(string inputString)
+        {
+            var sorted = Enumerable.Range('a', inputString.Max(c => c) - 'a' + 1).Select(i => (char)i).ToArray();
+            for (var i = 1; i < sorted.Length; i++)
+                if (inputString.Count(c => c == sorted[i]) > inputString.Count(c => c == sorted[i - 1]))
+                    return false;
+
+            return true;
+        }
+    }
+}
