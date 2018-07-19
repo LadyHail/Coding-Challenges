4ce71301298f6df3c2e68d43eccc90223c57cbe6
LadyHail <ladydeath@o2.pl>
2018-07-19 20:56:10 +0200

    CodeFights - Elections Winners

--- Main/CodeFights/IsBeautifulString.cs
+++ Main/CodeFights/IsBeautifulString.cs
@@ -1,5 +1,19 @@
 ﻿using System.Linq;
 
+/*
+ DESCRIPTION:
+ A string is said to be beautiful if b occurs in it no more times than a; c occurs in it no more times than b; etc.
+ Given a string, check whether it is beautiful.
+ Example
+    For inputString = "bbbaacdafe", the output should be
+    isBeautifulString(inputString) = true;
+    For inputString = "aabbb", the output should be
+    isBeautifulString(inputString) = false;
+    For inputString = "bbc", the output should be
+    isBeautifulString(inputString) = false.
+
+*/
+
 namespace Main.CodeFights
 {
     public static class IsBeautifulString
