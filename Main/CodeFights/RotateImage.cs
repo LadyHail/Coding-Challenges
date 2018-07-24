c20f3fa0fcaf9d09f8fd2834cea0b1aab85425b2
LadyHail <ladydeath@o2.pl>
2018-07-24 10:24:14 +0200

    CodeFights - rotateImage

---
+++ Main/CodeFights/RotateImage.cs
@@ -0,0 +1,30 @@
+﻿/*
+ DESCRIPTION:
+ Note: Try to solve this task in-place (with O(1) additional memory), since this is what you'll be asked to do during an interview.
+ You are given an n x n 2D matrix that represents an image. Rotate the image by 90 degrees (clockwise).
+ */
+
+namespace Main.CodeFights
+{
+    public static class RotateImage
+    {
+        public static int[][] rotateImage(int[][] a)
+        {
+            int[][] n = new int[a.Length][];
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                int[] inner = new int[a.Length];
+                int row = a.Length - 1;
+                for (int j = 0; j < a.Length; j++, row--)
+                {
+                    inner[j] = a[row][i];
+                }
+                n[i] = new int[inner.Length];
+                inner.CopyTo(n[i], 0);
+            }
+
+            return n;
+        }
+    }
+}
