5485c51cdc951279f17c5618a00383f798fbe793
LadyHail <ladydeath@o2.pl>
2018-07-29 16:32:21 +0200

    CodeFights - deleteDigit

---
+++ Main/CodeFights/DeleteDigit.cs
@@ -0,0 +1,32 @@
+﻿/*
+ DESCRIPTION: 
+ Given some integer, find the maximal number you can obtain by deleting exactly one digit of the given number.
+ Example
+    For n = 152, the output should be
+    deleteDigit(n) = 52;
+    For n = 1001, the output should be
+    deleteDigit(n) = 101.
+
+ */
+
+using System;
+
+namespace Main.CodeFights
+{
+    public static class DeleteDigit
+    {
+        public static int deleteDigit(int n)
+        {
+            string s = n.ToString();
+            int i = 0;
+            for (i = 0; i < s.Length - 1; i++)
+            {
+                if (s[i] < s[i + 1])
+                {
+                    break;
+                }
+            }
+            return Convert.ToInt32(s.Remove(i, 1));
+        }
+    }
+}
