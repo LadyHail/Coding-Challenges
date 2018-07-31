39a7ae58c8d5f51168ed8dccc4fc0c9837eba44e
LadyHail <ladydeath@o2.pl>
2018-07-31 19:32:11 +0200

    CodeFights - longestWord

---
+++ Main/CodeFights/LongestWord.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+
+/*
+ DESCRIPTION:
+ Define a word as a sequence of consecutive English letters. Find the longest word from the given string.
+ Example
+ For text = "Ready, steady, go!", the output should be
+ longestWord(text) = "steady".
+*/
+
+namespace Main.CodeFights
+{
+    public static class LongestWord
+    {
+        public static string longestWord(string text)
+        {
+            Regex reg = new Regex(@"([A-Za-z])+");
+            var matches = reg.Matches(text);
+            string max = "";
+
+            foreach (Match m in matches)
+            {
+                if (m.Length > max.Length)
+                {
+                    max = m.ToString();
+                }
+            }
+
+            return max;
+        }
+    }
+}
