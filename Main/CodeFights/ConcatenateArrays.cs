fce6c9ac0c7948a819752354084a000229318d7f
LadyHail <ladydeath@o2.pl>
2019-02-24 20:52:47 +0100

    CodeSignal - ConcatenateArrays

---
+++ Main/CodeFights/ConcatenateArrays.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+/*
+ DESCRIPTION: 
+ Given two arrays of integers a and b, obtain the array formed by the elements of a followed by the elements of b.
+
+ Example:
+ For a = [2, 2, 1] and b = [10, 11], the output should be
+ concatenateArrays(a, b) = [2, 2, 1, 10, 11].
+*/
+
+namespace Main.CodeFights
+{
+    public static class ConcatenateArrays
+    {
+        public static int[] concatenateArrays(int[] a, int[] b)
+        {
+            return a.Concat(b).ToArray();
+        }
+    }
+}
