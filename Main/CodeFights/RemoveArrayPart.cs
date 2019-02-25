18cc9e6948b2d53b29bac5a5e244d164691794e9
LadyHail <ladydeath@o2.pl>
2019-02-25 19:32:08 +0100

    CodeSignal - RemovePartArray

---
+++ Main/CodeFights/RemoveArrayPart.cs
@@ -0,0 +1,21 @@
+﻿/*
+ DESCRIPTION: 
+ Remove a part of a given array between given 0-based indexes l and r (inclusive).
+
+ Example:
+ For inputArray = [2, 3, 2, 3, 4, 5], l = 2, and r = 4, the output should be
+ removeArrayPart(inputArray, l, r) = [2, 3, 5].
+*/
+
+using System.Linq;
+
+namespace Main.CodeFights
+{
+    public static class RemoveArrayPart
+    {
+        public static int[] removeArrayPart(int[] inputArray, int l, int r)
+        {
+            return inputArray.Where((x, y) => y < l || y > r).ToArray();
+        }
+    }
+}
