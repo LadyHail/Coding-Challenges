a120d1360e7a0bd44b4cd9504e752e7b98411299
LadyHail <ladydeath@o2.pl>
2019-02-28 19:47:20 +0100

    CodeSignal - IsSmooth

--- Main/_Main.cs
+++ Main/_Main.cs
@@ -7,7 +7,7 @@ namespace Main
     {
         private static void Main(string[] args)
         {
-            CreateArray.createArray(4);
+            IsSmooth.isSmooth(new int[] { 7, 2, 2, 5, 10, 7 });
 
             //Console.WriteLine(MessageFromBinaryCode.messageFromBinaryCode(input));
             Console.ReadKey();
