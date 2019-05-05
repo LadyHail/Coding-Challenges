cdff26446166b5562fb29fcf1b55ea4cf80bf5c0
LadyHail <ladydeath@o2.pl>
2019-05-05 21:39:58 +0200

    Codility - Cyclic Rotation

---
+++ Main/Codility/CyclicRotation.cs
@@ -0,0 +1,34 @@
+﻿namespace Main.Codility
+{
+    public static class CyclicRotation
+    {
+        public static int[] cyclicRotation(int[] A, int K)
+        {
+            if (A.Length < 2)
+            {
+                return A;
+            }
+
+            int[] r = new int[A.Length];
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (i + K > A.Length)
+                {
+                    int index = (i + K) % A.Length;
+                    r[index] = A[i];
+                }
+                else if (i + K == A.Length)
+                {
+                    r[0] = A[i];
+                }
+                else
+                {
+                    r[i + K] = A[i];
+                }
+            }
+
+            return r;
+        }
+    }
+}
