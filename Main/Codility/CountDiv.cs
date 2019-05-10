bed2ebc12adc5cf1b8018d374585c0032cec2fd9
LadyHail <ladydeath@o2.pl>
2019-05-10 20:50:17 +0200

    Codility - Count Div

---
+++ Main/Codility/CountDiv.cs
@@ -0,0 +1,16 @@
+﻿namespace Main.Codility
+{
+    public class CountDiv
+    {
+        public static int Solve(int A, int B, int K)
+        {
+            int b = B / K;
+            int a = (A > 0 ? (A - 1) / K : 0);
+            if (A == 0)
+            {
+                b++;
+            }
+            return b - a;
+        }
+    }
+}
