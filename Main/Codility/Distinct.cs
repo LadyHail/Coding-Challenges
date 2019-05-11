a26a5fdb0e4fbd447807177427e5ba197b2c9687
LadyHail <ladydeath@o2.pl>
2019-05-11 23:09:55 +0200

    Codility - Distnct

---
+++ Main/Codility/Distinct.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace Main.Codility
+{
+    public class Distinct
+    {
+        public static int Solve(int[] A)
+        {
+            return A.GroupBy(x => x).Count();
+        }
+    }
+}
